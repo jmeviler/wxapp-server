8408beb17b04169e275be75f63105a2a322f2704
Ben Li <benli@augmentum.com.cn>
2016-10-21 18:47:01 +0800

    add cloud token

--- cloud.js
+++ cloud.js
@@ -12,7 +12,28 @@ AV.Cloud.define('dailyWeather', function(request, response) {
 });
 
 AV.Cloud.define('token', function(request, response) {
-  token();
+  var options = {
+    uri: 'https://api.weixin.qq.com/cgi-bin/token',
+    qs: {
+      grant_type: 'client_credential',
+      appid: APPID,
+      secret: SecretKey
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  };
+
+  rp(options)
+  .then(function (parsedBody) {
+    GLOBAL.token = parsedBody.access_token;
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+  console.error(GLOBAL.token);
+  response.success(GLOBAL.token);
 });
 
 module.exports = AV.Cloud;
