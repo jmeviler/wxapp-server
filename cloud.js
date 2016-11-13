f0d4bd7dfa37b5e5654b3e25ac20171a9cf01c35
BenLi <BenLi@LiBendeMacBook-Pro.local>
2016-11-13 10:17:52 +0800

    update template with error

--- cloud.js
+++ cloud.js
@@ -41,7 +41,7 @@ AV.Cloud.define('dailyWeather', function(req, response) {
 });
 
 function sendWeather (error, res, body) {
-  console.error(error);
+  console.error(error, res, body);
   var resData = JSON.parse(body);
   var tipt = resData.results[0].index[0];
   var weather = resData.results[0].weather_data[0];
