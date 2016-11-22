f8c13286b2376f4fc9a02310b45742d4a596a1d3
Ben Li <benli@augmentum.com.cn>
2016-11-22 19:57:05 +0800

    update the function

--- routes/noteBook.js
+++ routes/noteBook.js
@@ -2,17 +2,13 @@ var AV = require('leanengine');
 var request = require('request');
 var router = require('express').Router();
 
-// var APP_ID = process.env.APP_ID;
-// var APP_KEY = process.env.APP_KEY;
-
-// AV.init({ appId: APP_ID, appKey: APP_KEY });
-
 router.get('/add', function(req, res, next) {
-  var NoteBook = AV.Object.extend('TestObject');
+  var NoteBook = AV.Object.extend('noteBook');
   var boodBook = new NoteBook();
   boodBook.save({
     note: 'Hello World!'
   }).then(function(object) {
+    res.send('note save success');
     console.error('note save success');
   })
 });
