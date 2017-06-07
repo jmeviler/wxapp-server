c3a2c74f4bc1c305b8bca8225f99847d609b75c6
Ben Li <admin@onesway.xyz>
2017-06-07 21:30:36 +0800

    add config

--- routes/jssdk.js
+++ routes/jssdk.js
@@ -1,10 +1,23 @@
 var request = require('request');
 var router = require('express').Router();
 var logger = require('log4js').getLogger();
+var wechat = require('wechat');
+
+var TOKEN = process.env.TOKEN;
+var APPID = process.env.APPID;
+var EncodingAESKey = process.env.EncodingAESKey;
+var SecretKey = process.env.SecretKey;
+
+var config = {
+  token: TOKEN,
+  appid: APPID,
+  encodingAESKey: EncodingAESKey
+};
+
+var WechatAPI = require('wechat-api');
+var api = new WechatAPI(APPID, SecretKey);
 
 router.get('/jsconfig', function(req, res, next) {
-  var url = req.params.url;
-  logger.debug(url, '---config url');
   var param = {
     debug: false,
     jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'],
